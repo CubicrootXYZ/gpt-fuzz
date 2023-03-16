acbb9fd2d8300bbd07e58efc8cb1e2a3656de039
cubicroot <github@cubicroot.xyz>
2023-03-16 20:42:27 +0100

    first working prototype

---
+++ run.py
@@ -0,0 +1,19 @@
+import atheris
+from transformers import pipeline
+
+with atheris.instrument_imports():
+  import sys
+
+gen = pipeline('text-generation', model ='EleutherAI/gpt-neo-125M')
+
+def testFnc(data):
+    try:
+        input = data.decode("utf-8")
+    except:
+       # Failed to utf8 decode, skip this one
+       return
+    output = gen(input, max_length=100, do_sample=True, temperature=0.9)
+    print(f"INPUT: {input}; OUTPUT: {output[0]['generated_text']}")
+
+atheris.Setup(sys.argv, testFnc)
+atheris.Fuzz()
