0e37a5e45c5f68bd610f533a64276f11b1f53f71
cubicroot <github@cubicroot.xyz>
2023-03-17 20:34:09 +0100

    write to CSV and make ARM compatible

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.0 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-pip install transformers atheris
+pip install transformers
--- run.py
+++ run.py
@@ -1,23 +1,49 @@
-import atheris
+import csv
+import random
+import traceback
+
+import transformers
 from transformers import pipeline
 
-with atheris.instrument_imports():
-    import sys
+transformers.logging.set_verbosity_error()
+
+all_letters = [" ", " ", " ", " ", " ", " ", " ", ".", ".", ".", ".", ";", ":"]
+
+for i in range(0, 1100):
+    all_letters.append(chr(i))
 
 gen = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
 
+j = 0
+f = open("tests.csv", "a")
+writer = csv.writer(f)
+
 
 def testFnc(data):
+    global j
+    global f
+    j += 1
+    output = gen(data, max_length=100, do_sample=True, temperature=0.9)
+    writer.writerow([data, output[0]["generated_text"]])
+
+    if j % 100 == 0:
+        print(f"{j} inputs tested")
+        # Write to file in case we fail at some point
+        f.close
+        f = open("tests.csv", "wb")
+
+
+def get_random_string(length):
+    result_str = "".join(random.choice(all_letters) for i in range(length))
+    return result_str
+
+
+while True:
+    input = get_random_string(random.randint(1, 100))
     try:
-        input = data.decode("utf-8")
-    except:
-        # Failed to utf8 decode, skip this one
-        return
-    output = gen(input, max_length=100, do_sample=True, temperature=0.9)
-    # TODO store in CSV or so, make sure encoding works xD
-    print(f"INPUT: {input}; OUTPUT: {output[0]['generated_text']}")
-
-
-atheris.Setup(sys.argv, testFnc)
-atheris.Fuzz()
-# TODO multithread, or is it already?
+        testFnc(input)
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+        print(f"Failed with input: {input}")
+        break
