7f969bab883100ec17b1c7b57ff4e68befe7e460
cubicroot <github@cubicroot.xyz>
2023-03-17 20:59:00 +0100

    add some code annotations

--- run.py
+++ run.py
@@ -8,18 +8,21 @@ from transformers import pipeline
 
 transformers.logging.set_verbosity_error()
 
+# Get the base letters, increase likelihood of spaces, etc.
 all_letters = [" ", " ", " ", " ", " ", " ", " ", ".", ".", ".", ".", ";", ":"]
-
 for i in range(0, 1100):
     all_letters.append(chr(i))
 
+# Load model
 gen = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
 
+# Prepare foo
 j = 0
 f = open("tests.csv", "a")
 writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
 
+# The function that is fuzzed
 def testFnc(data):
     global j
     global f
@@ -35,11 +38,13 @@ def testFnc(data):
         time.sleep(2)  # let's hope that's enough for the hoster to be fine
 
 
+# Random string generator
 def get_random_string(length):
     result_str = "".join(random.choice(all_letters) for i in range(length))
     return result_str
 
 
+# Run forever xD
 while True:
     input = get_random_string(random.randint(1, 100))
     try:
